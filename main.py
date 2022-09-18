30caf1606135f7509cf27a8c04ed30c34a0498fb
nsternbergh <111912713+nsternbergh@users.noreply.github.com>
2022-09-18 03:38:43 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+male_students = int(input('Enter male students'))
+female_students = int(input('Enter female students'))
+total_students = int(input('Enter total students'))
+
+fp = female_students / total_students
+mp = male_students / total_students
+
+print("Female Students {}". format(fp))
+print("Male students {}". format(mp))
+print("Total students {}". format(total_students))
+
